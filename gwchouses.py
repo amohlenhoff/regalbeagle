0be1b983eaf3a618e7ea548ac9802b897430d643
Jonathan Mohlenhoff <jonathan.mohlenhoff@nulinesensors.com>
2016-08-03 08:51:03 -0400

    tried making .wav files work on all OS platforms with QT

--- gwchouses.py
+++ gwchouses.py
@@ -6,110 +6,84 @@ Created on Wed Jul 13 17:04:09 2016
 """
 
 import random
-import subprocess
+import winsound
 import time
 import os
+from PyQt4 import QtCore, QtGui, uic
+import sys
+import Queue
 
-projectRoot = os.path.dirname(os.path.abspath(__file__))
+class SoundPlayer(QtCore.QObject):
+    
+    def __init__(self, parent=None):
+        super(self.__class__, self).__init__(parent)
+        self.soundQueue = Queue.Queue()
+        self.playing = True
+        self.currentSound = None
+        self.workerThread = QtCore.QThread()
+        self.moveToThread(self.workerThread)
+        self.workerThread.start()
+        
+    @QtCore.pyqtSlot()
+    def startWork(self):
+        while self.playing:
+            if self.currentSound == None:
+                if not self.soundQueue.empty():
+                    self.currentSound = QtGui.QSound(self.soundQueue.get())
+                    self.currentSound.setLoops(1)
+                    self.currentSound.play()
+            else:
+                if self.currentSound.isFinished():
+                    self.currentSound = None
+            time.sleep(0.25)
+            
+    def playSound(self, fileName):
+        self.soundQueue.put(fileName)
+
+projectRoot = os.path.dirname(__file__)
+sp = SoundPlayer()
 def PlayStall(stallNum):
     if stallNum == 1:
-        subprocess.call(['afplay', projectRoot + '/stalling/ahright.wav'])
+        sp.playSound(projectRoot + '/stalling/ahright.wav')
+        #QtGui.QSound.play(projectRoot + '/stalling/ahright.wav')
     elif stallNum == 2:
-        subprocess.call(['afplay', projectRoot + '/stalling/difficult.wav'])
+        sp.playSound(projectRoot + '/stalling/difficult.wav')
+        #QtGui.QSound.play(projectRoot + '/stalling/difficult.wav')  
     elif stallNum == 3:
-        subprocess.call(['afplay', projectRoot + '/stalling/itsallhere.wav'])
+        sp.playSound(projectRoot + '/stalling/itsallhere.wav')
+        #QtGui.QSound.play(projectRoot + '/stalling/itsallhere.wav') 
     elif stallNum == 4:
-        subprocess.call(['afplay', projectRoot + '/stalling/rightok.wav'])
+        sp.playSound(projectRoot + '/stalling/rightok.wav')
+        #QtGui.QSound.play(projectRoot + '/stalling/rightok.wav')  
     else:
-        subprocess.call(['afplay', projectRoot + '/stalling/wheretoputyou.wav'])
-
+        sp.playSound(projectRoot + '/stalling/wheretoputyou.wav')
+        #QtGui.QSound.play(projectRoot + '/stalling/wheretoputyou.wav')          
+        
 def PlayHouse(HouseName):
-    if HouseName == 'Hopper':
-        print 'Welcome to House Hopper %s!' % (name)
-    elif HouseName == 'Lovelace':
-        print 'Welcome to House Lovelace %s!' % (name)
-    elif HouseName == 'Borg':
-        print 'Welcome to House Borg %s!' % (name)
+    '''if HouseName == 'Gryffindor':
+        winsound.PlaySound(projectRoot + '/houses/gryffindor.wav', winsound.SND_FILENAME)
+        #winsound.PlaySound('C:/Users/Developer/Desktop/SortingHat/houses/gryffindor.wav', winsound.SND_FILENAME)
+    elif HouseName == 'Ravenclaw':
+        winsound.PlaySound(projectRoot + '/houses/ravenclaw.wav', winsound.SND_FILENAME)        
+        #winsound.PlaySound('C:/Users/Developer/Desktop/SortingHat/houses/ravenclaw.wav', winsound.SND_FILENAME)
+    elif HouseName == 'Hufflepuff':
+        winsound.PlaySound(projectRoot + '/houses/hufflepuff.wav', winsound.SND_FILENAME)
+        #winsound.PlaySound('C:/Users/Developer/Desktop/SortingHat/houses/hufflepuff.wav', winsound.SND_FILENAME)
     else:
-        print 'Welcome to House Fried %s!' % (name)
-
+        winsound.PlaySound(projectRoot + '/houses/slytherin.wav', winsound.SND_FILENAME)
+        #winsound.PlaySound('C:/Users/Developer/Desktop/SortingHat/houses/slytherin.wav', winsound.SND_FILENAME)'''
+        
 def PlayKnow(KnowNum):
     if KnowNum == 0:
         time.sleep(0)
     elif KnowNum == 1:
-        subprocess.call(['afplay', projectRoot + '/know/iknow.wav'])
+        sp.playSound(projectRoot + '/know/iknow.wav')
+        #QtGui.QSound.play(projectRoot + '/know/iknow.wav')
     else:
-        subprocess.call(['afplay', projectRoot + '/know/iknowjustwhattodo.wav'])
+        sp.playSound(projectRoot + '/know/iknowjustwhattodo.wav')
+        #QtGui.QSound.play(projectRoot + '/know/iknowjustwhattodo.wav')
 
-
-number_of_girls = input("How many girls are in the camp? ")
-houses = ['Hopper', 'Lovelace', 'Borg', 'Fried']
-girls = []
-stalls = [1, 2, 3, 4, 5]
-random_stalls = []
-iknows = [0, 1, 2]
-iknows_random = []
-names = []
-
-for i in range(number_of_girls):
-    girls.append(houses[i%len(houses)])
-    random_stalls.append(stalls[i%len(stalls)])
-    iknows_random.append(iknows[i%len(iknows)])
-random.shuffle(girls)
-random.shuffle(random_stalls)
-random.shuffle(iknows_random)
-
-print girls
-print random_stalls
-print iknows_random
-
-for i in range(number_of_girls):
-    name = raw_input("What is your name campee? ")
-    names.append(name[i])
-    PlayStall(random_stalls[i])
-    time.sleep(1)
-    PlayKnow(iknows_random[i])
-    time.sleep(0.25)
-    PlayHouse(girls[i])
-    time.sleep(3)
-
-from PyQt4 import QtCore, QtGui
-import sys
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
-
-class Ui_Form(QtGui.QWidget):
-    def __init__(self):
-	QtGui.QWidget.__init__(self)
-	self.setupUi(self)
-
-    def setupUi(self, Dialog):
-        Dialog.setObjectName(_fromUtf8("Dialog"))
-        Dialog.resize(584, 384)
-        self.horizontalLayout = QtGui.QHBoxLayout(Dialog)
-        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.verticalLayout = QtGui.QVBoxLayout()
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.sorts_girls_btn = QtGui.QPushButton(Dialog)
-        self.sorts_girls_btn.setObjectName(_fromUtf8("sorts_girls_btn"))
-        self.verticalLayout.addWidget(self.sorts_girls_btn)
-        self.horizontalLayout.addLayout(self.verticalLayout)
-
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-
+'''   
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(_translate("Dialog", "Sorting Hat", None))
         self.sorts_girls_btn.setText(_translate("Dialog", "Let the Sorting Begin...", None))
@@ -117,18 +91,48 @@ class Ui_Form(QtGui.QWidget):
 
     def SortGirls(self):
         for i in range(number_of_girls):
-            PlayStall(random_stalls[i])
-            time.sleep(1)
-            PlayKnow(iknows_random[i])
-            time.sleep(0.25)
-            PlayHouse(girls[i])
-            time.sleep(3)
+            
+'''
+
+def SortButton():
+    global girl_index
+    print 'sort button'
+    PlayStall(random_stalls[girl_index])
+    time.sleep(1)
+    PlayKnow(iknows_random[girl_index])
+    time.sleep(0.25)
+    PlayHouse(girls[girl_index])
+    time.sleep(3) 
+    girl_index += 1
 
 if __name__ == '__main__':
+    number_of_girls = input("How many girls are in the camp? ")
+    houses = ['Gryffindor', 'Ravenclaw', 'Hufflepuff', 'Slytherin']
+    girls = []
+    stalls = [1, 2, 3, 4, 5]
+    random_stalls = []
+    iknows = [0, 1, 2]
+    iknows_random = []
+    girl_index = 0
+
+    for i in range(number_of_girls):
+        girls.append(houses[i%len(houses)])
+        random_stalls.append(stalls[i%len(stalls)])
+        iknows_random.append(iknows[i%len(iknows)])
+    random.shuffle(girls)
+    random.shuffle(random_stalls) 
+    random.shuffle(iknows_random)
+    
+    print girls   
+    print random_stalls
+    print iknows_random
+    
     app = QtGui.QApplication(sys.argv)
-    ex = Ui_Form()
-    ex.show()
+    timer = QtCore.QTimer()
+    timer.timeout.connect(sp.startWork)
+    timer.start(0)
+    win = uic.loadUi('UI/GUI.ui')
+    win.sorts_girls_btn.clicked.connect(SortButton)    
+    
+    win.show()    
     sys.exit(app.exec_())
-
-
-
